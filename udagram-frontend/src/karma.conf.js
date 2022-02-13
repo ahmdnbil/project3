e9b731e5564549bc20937b0c92b3c206fad5ae12
ahmdnbil <batmoon555@gmail.com>
2022-02-13 21:21:55 +0200

    last commit

--- udagram-frontend/src/karma.conf.js
+++ udagram-frontend/src/karma.conf.js
@@ -2,6 +2,7 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+  const process = require("process");
   process.env.CHROME_BIN = require("puppeteer").executablePath();
   config.set({
     basePath: "",
@@ -27,6 +28,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ["Chrome"],
-    singleRun: false,
+    singleRun: true,
+    concurrency: Infinity,
   });
 };
